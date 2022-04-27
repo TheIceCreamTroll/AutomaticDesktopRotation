310f43fa03c3286e88fafd193a0c757480163d8c
Luka Specter <lukaspecter@gmail.com>
2022-04-27 14:54:27 -0400

    Added back and improved display.exe implementation

--- AutomaticDesktopRotation.py
+++ AutomaticDesktopRotation.py
@@ -11,6 +11,9 @@ import serial.tools.list_ports as listports
 # Number of Arduinos being used.
 DeviceCount = 2
 
+# If, for whatever reason, the win32api rotation method doesn't work, this is a functionally equivalent fallback
+windowsfallback = True
+displayexe = "display64.exe" # Path to display64.exe
 # Enable this to print stuff to console. Wouldn't do anything while running in the background.
 quiet = True
 
@@ -18,8 +21,9 @@ quiet = True
 
 # The OS determines which method we use to rotate the screen.
 if platform.startswith('win32'):
-    import win32api as win32
-    import win32con
+    if not windowsfallback:
+        import win32api as win32
+        import win32con
     operatingSystem = "windows"
 else:
     operatingSystem = "linux"  # OSX can use xrandr via MacPorts. FreeBSD should be able to use it as well.
@@ -60,8 +64,6 @@ def waitForSerialInit():
 
 def RotationProcess():
 
-    # Initialize at 0 degrees
-    #old = "0"
     ser = waitForSerialInit()
 
     while True:
@@ -81,7 +83,7 @@ def RotationProcess():
                 line = line.strip().split()
                 log(line)
 
-                displayID = int(line[0]) - 1
+                displayID = int(line[0])
                 angle = float(line[1])
 
                 if angle <= -40:
@@ -97,27 +99,28 @@ def RotationProcess():
                     current = "90"
 
                 if operatingSystem == "windows":
-                    device = win32.EnumDisplayDevices(None, displayID)
-                    dm = win32.EnumDisplaySettings(device.DeviceName, win32con.ENUM_CURRENT_SETTINGS)
+                    if windowsfallback:
+                        run(f"{displayexe} /device {displayID} /rotate {current} /display none", shell=True)
+                    else:
+                        device = win32.EnumDisplayDevices(None, displayID - 1)
+                        dm = win32.EnumDisplaySettings(device.DeviceName, win32con.ENUM_CURRENT_SETTINGS)
 
-                    if current == "0":
-                        log("Rotating to 0 degrees")
-                        dm.DisplayOrientation = win32con.DMDO_DEFAULT
-                    elif current == "90":
-                        log("Rotating to 90 degrees")
-                        dm.DisplayOrientation = win32con.DMDO_90
-                    elif current == "270":
-                        log("Rotating to 270 degrees")
-                        dm.DisplayOrientation = win32con.DMDO_270
+                        if current == "0":
+                            log("Rotating to 0 degrees")
+                            dm.DisplayOrientation = win32con.DMDO_DEFAULT
+                        elif current == "90":
+                            log("Rotating to 90 degrees")
+                            dm.DisplayOrientation = win32con.DMDO_90
+                        elif current == "270":
+                            log("Rotating to 270 degrees")
+                            dm.DisplayOrientation = win32con.DMDO_270
 
-                    dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth
-                    win32.ChangeDisplaySettingsEx(device.DeviceName, dm)
+                        dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth
+                        win32.ChangeDisplaySettingsEx(device.DeviceName, dm)
 
                 else: # I (do not) use Arch btw, so this may not be the most optimal solution, or even work.
                     run(f"xrandr --output HDMI1 --rotate {current} &", shell=True)  # --screen [deviceID] ?
 
-            #old = current
-            
             # Turning a monitor off will cause an exception to be thrown.
             except Exception as e:
                 log(e)
